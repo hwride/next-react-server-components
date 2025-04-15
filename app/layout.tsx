c5a0a2d528953e11a4835a5205d04b49b06a6d71
Hugo Wride <hugowride@gmail.com>
2025-04-15 08:38:31 +0100

    Remove some boilerplate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,5 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +14,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className='antialiased p-4'
       >
+        This is <code>layout.tsx</code>
         {children}
       </body>
     </html>

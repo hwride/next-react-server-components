551e24e1f50721e623f15b6a8b16fcda8147ba9d
Hugo Wride <hugowride@gmail.com>
2025-04-18 09:35:24 +0100

    Style page names a bit

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
     return (
         <main className='border p-4 flex flex-col'>
-            <Image
-                className="dark:invert"
-                src="/next.svg"
-                alt="Next.js logo"
-                width={180}
-                height={38}
-                priority
-            />
-            <div>This is <code>app/page.tsx</code></div>
+            <div>This is <code className='code-bright'>app/page.tsx</code></div>
             <div>{new Date().toISOString()}</div>
             <a href='/static'>/static - web link</a>
             <Link href='/static'>/static - Next <code>{`<`}Link{`>`}</code></Link>
--- app/static/page.tsx
+++ app/static/page.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
 export default function Test1Page() {
-    return <div className='flex flex-col'>
-        <div>This is <code>static/page.tsx</code></div>
+    return <div className='border p-4 flex flex-col'>
+        <div>This is <code className='code-bright'>static/page.tsx</code></div>
         <div>{new Date().toISOString()}</div>
         <a href='/'>/ - web link</a>
         <Link href='/'>/ - Next <code>{`<`}Link{`>`}</code></Link>
